7a735084d969c3fca2c924119873faaa77a393f5
蓝海互联 <yangjingjing@bluehi.cn>
2016-05-05 14:51:31 +0800

    Single上传
    Single第一次上传

---
+++ Single/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Single
+//
+//  Created by shangbang on 16/5/5.
+//  Copyright © 2016年 www.bluehi.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Single/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Single
+//
+//  Created by shangbang on 16/5/5.
+//  Copyright © 2016年 www.bluehi.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
